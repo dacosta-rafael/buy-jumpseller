e03c90f9ac0c4bf033bad77f0b58ebc8fbbecb58
rafael da costa <rafaeldacosta@rafaels-MBP.iptv.microsoft.com>
2016-09-26 13:00:19 -0300

    using classes

--- jumpseller-buy/jumpseller-buy.php
+++ jumpseller-buy/jumpseller-buy.php
@@ -12,45 +12,72 @@ Author URI:
 
 */
 
-//tell wordpress to register the jumpseller shortcode
-add_shortcode("jumpseller", "jumpseller_handler");
-
-function jumpseller_handler( $atts ) {
-          $_output = jumpseller_parse($atts);
-          //$storecode = 'pihaus';
-          //include 'tmp.php';
-          return $_output;
-}
-
-function jumpseller_parse($atts) {
-		$product_id = $atts['product_id'];
-		$storecode = 'pihaus';
-		$storetoken = 'FFIPTJVUHAMBUJUILADGGNOLTSQANSCD';
-		$call = curl_call( $product_id, $storecode, $storetoken) ;
-		$json_data =   json_decode($call, true);
-		///munge file here and save it
-	  	return $json_data;
-}
-
-function curl_call($product_id, $storecode, $storetoken) {
-	$url = 'https://api.jumpseller.com/v1/products/'.$product_id.'.json?login='.$storecode.'&authtoken='.$storetoken;
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-		'Content-Type: application/json',
-		)
-	);
-	$result = curl_exec($ch);
-		// Check HTTP status code
-		if (!curl_errno($ch)) {
-		  switch ($http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
-		    case 200:  # OK
-		      break;
-		    default:
-		      echo 'Unexpected HTTP code: ', $http_code, "\n";
-		  }
+class Model {
+		public function jumpseller_parse($atts, $storecode, $storetoken) {
+				$product_id = $atts['product_id'];
+				$call = $this->curl_call( $product_id, $storecode, $storetoken) ;
+				$json_data =   json_decode($call, true);
+				///munge file here and save it
+			  	return $json_data;
 		}
-	curl_close($ch);
-	return $result;
+
+		public function curl_call($product_id, $storecode, $storetoken) {
+			$url = 'https://api.jumpseller.com/v1/products/'.$product_id.'.json?login='.$storecode.'&authtoken='.$storetoken;
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+				'Content-Type: application/json',
+				)
+			);
+			$result = curl_exec($ch);
+				// Check HTTP status code
+				if (!curl_errno($ch)) {
+				  switch ($http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
+				    case 200:  # OK
+				      break;
+				    default:
+				      echo 'Unexpected HTTP code: ', $http_code, "\n";
+				  }
+				}
+			curl_close($ch);
+			return $result;
+		}
 }
+
+
+class Controller {
+     public $model;	
+     public function __construct($storecode, $storetoken)  
+     {  
+     	            $this->model = new Model();
+     	            $this->storecode = $storecode;
+     	            $this->storetoken = $storetoken;
+     } 
+     public function jumpseller_handler( $atts ) {
+          		$_output = $this->model->jumpseller_parse($atts, $this->storecode, $this->storetoken);
+          		//print_r( $_output  );
+          		ob_start();
+          		include 'tmp.php';
+          		return ob_get_clean();
+	} 
+}
+
+$storecode = 'pihaus';
+$storetoken = 'FFIPTJVUHAMBUJUILADGGNOLTSQANSCD';
+//instatiate functions
+$controller = new Controller($storecode, $storetoken);
+//tell wordpress to register the jumpseller shortcode
+add_shortcode( 'jumpseller', array( $controller, 'jumpseller_handler' ) );
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- jumpseller-buy/tmp.php
+++ jumpseller-buy/tmp.php
@@ -1,5 +1,22 @@
-	
 
- https://<?php echo $storecode;?>.jumpseller.com/<?php echo $_output['product']['permalink'];?>
+	
+<div style="margin:5px auto; padding: 10px; clear:both; max-width:100px;text-align:center;border: 1px solid #4CAF50;">
+
+<a style="text-decoration:none;text-align:center;" href=" https://<?php echo $storecode;?>.jumpseller.com/<?php echo $_output['product']['permalink'];?>" target="_blank">
+	
+	<div style="width:100%; text-align:center;">
+		<img style="max-width:90px" src="<?php echo $_output['product']['images'][0]['url'];?>" />
+	</div>	
+
+	<div style="max-width:90px;margin:5px auto; text-align:center;max-height:50px; overflow:hidden;">
+		<?php echo $_output['product']['name'];?>
+	</div>
+
+	<input type="button" style="background-color: #4CAF50;border: none;color: white;padding: 10px 20px;text-align: center;text-decoration: none;display: inline-block;font-size: 14px;margin: 4px 2px;cursor: pointer;" value="Buy" />
+
+</a>
+	<div style="font-size:10px;line-height:normal; text-decoration:none;text-align:center; margin:5px 0 0 0">Powered by <a style="text-decoration:none;" href="https://jumpseller.com" target="_blank">Jumpseller</a>
+	</div>
+</div>
 
 	
